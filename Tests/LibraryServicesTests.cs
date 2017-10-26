605e350985f810af89f7b71d062f3b08228a8136
Eyrún <eyrun15@ru.is>
2017-10-26 15:10:08 +0000

    travis pastrana NR 6 :) :) :) :) :)

---
+++ Tests/LibraryServicesTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace API.Tests
+{
+    [TestClass]
+    public class LibraryServicesTests
+    {
+        private MockUnitOfWork<MockDataContext> _mockUnitOfWork;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
